b42e6ef22d764fbc3219b390d10ddd4b109c07a8
Christian Volk <christian.volk@ars-blue.at>
2021-02-04 18:40:43 +0100

    bug fix logging

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
  * Cron server for workers
  */
 
+const fs = require('fs');
+const path = require('path');
 const Configstore = require('configstore');
 const { createLogger, format, transports } = require('winston');
 require('winston-daily-rotate-file');
 const Graceful = require('@ladjs/graceful');
 const Bree = require('bree');
+
+const dir = path.dirname(fs.realpathSync(__filename));
+process.chdir(dir); // otherwise node-services/deamon-folder will be created in current directory!
 
 const config = new Configstore('voca-bau-node-services');
 if (!config.get('server')) {
